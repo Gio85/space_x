ad5ffaeb24a19a48af089226c545307914d5e3e0
Gio85 <giovanni.galiero85@gmail.com>
2020-12-17 18:02:09 +0000

    refactor: add basename to the router

--- src/components/app/providers/Router.tsx
+++ src/components/app/providers/Router.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from '../../pages/Home'
 
 export const AppRouterProvider: React.FC = () => (
-  <Router basename={'http://Gio85.github.io/space_x'}>
+  <Router basename={window.location.pathname || ''}>
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
